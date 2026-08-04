1f64d10c123f9b803e217d33cc632eb299ac39a4
wisdom <1071970200@qq.com>
2026-08-04 13:57:14 +0800

    fix: 撤回无效XML属性，改用MainActivity.java代码启用WebView功能
    javaScriptEnabled/domStorageEnabled不是XML属性，必须用Java代码设置
    同时在MainActivity.onStart()中显式配置WebView安全设置

--- android/app/src/main/java/com/wisdom/zhijuan/MainActivity.java
+++ android/app/src/main/java/com/wisdom/zhijuan/MainActivity.java
@@ -1,5 +1,24 @@
 package com.wisdom.zhijuan;
 
+import android.os.Bundle;
+import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onStart() {
+        super.onStart();
+        try {
+            WebView webView = getBridge().getWebView();
+            webView.getSettings().setJavaScriptEnabled(true);
+            webView.getSettings().setDomStorageEnabled(true);
+            webView.getSettings().setDatabaseEnabled(true);
+            webView.getSettings().setAllowFileAccess(true);
+            webView.getSettings().setAllowContentAccess(true);
+            // OPPO/ColorOS: 显式设置 WebView 渲染模式
+            webView.getSettings().setMixedContentMode(0); // MIXED_CONTENT_ALWAYS_ALLOW
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
